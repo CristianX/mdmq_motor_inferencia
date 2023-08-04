541b5bb087e93ad198546d6914fc298582e25f99
Cristian Xavier Tapia Vargas <thecristianx@hotmail.com>
2023-08-04 09:37:10 -0500

    Modificando el consumo cors

--- mdmq_motor_inferencia/settings.py
+++ mdmq_motor_inferencia/settings.py
@@ -145,6 +145,6 @@ STATIC_URL = "static/"
 DEFAULT_AUTO_FIELD = "django.db.models.BigAutoField"
 
 # Configuración CORS
-CORS_ALLOW_ALL_ORIGINS = False
+CORS_ALLOW_ALL_ORIGINS = True
 
-CORS_ALLOWED_ORIGINS = [config("URL_ALLOW_FRONTEND")]
+# CORS_ALLOWED_ORIGINS = [config("URL_ALLOW_FRONTEND")]
